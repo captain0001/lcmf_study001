8fb9826e509b67fd425796c069c9c9c4a2e3a6b4
J13281 <tuis.j13281@gmail.com>
2020-03-28 15:52:28 +0900

    table 作成

---
+++ lcmfhq_study001/src/components/MyTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <table class="table bg-white">
+      <thead class="thead-dark">
+      <tr>
+        <th v-for="x of dataset.headers" :key="x">{{ x }}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="xs of dataset.data" :key="xs">
+        <td v-for="x of xs" :key="x">{{ x }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTable',
+  data() { return {
+    dataset: {
+      "headers": ["No.", "名前", "ジャンル", "楽器"],
+      "data": [
+        ["1", "yasuhiro", "all", "guitar"],
+        ["2", "ob_yuu", "electoro", "piano"],
+        ["3", "gorio", "orchestra", "-"],
+        ["4", "kuarisu", "electro", "-"],
+        ["5", "koudai", "rock", "piano"]
+      ]
+    }
+  }; },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- lcmfhq_study001/src/views/About.vue
+++ lcmfhq_study001/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <MyTable />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import MyTable from '@/components/MyTable.vue'
+
+export default {
+  name: 'About',
+  components: {
+    MyTable
+  }
+}
+</script>
